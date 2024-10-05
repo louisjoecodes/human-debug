4a9cb1e9a73cf2646c342096a1dc6146f7688ce4
louisjoecodes <louisjoecodes@gmail.com>
2024-10-05 14:12:33 +0100

    feat: fix build script

--- backend/src/main.py
+++ backend/src/main.py
@@ -131,7 +131,7 @@ def encode_image(image_path):
         return None
 
 def start():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
 
 if __name__ == "__main__":
     start()
