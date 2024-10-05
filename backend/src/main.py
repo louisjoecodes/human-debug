fd3c79f43d29e7b6b8aad4627e3602e69eef3a26
leandermaerkisch <leander.maerkisch@floy.com>
2024-10-05 11:54:32 +0100

    Implement image scan to transcription api endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,19 +1,24 @@
 import os
-import asyncio
-import json
+
+import base64
+from fastapi import UploadFile, File
+from mistralai import Mistral
+
+
+import utils
 
 from dotenv import load_dotenv
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Initialize the FastAPI app
+client = Mistral(api_key=os.getenv("MISTRAL_API_KEY"))
+
 app = FastAPI(
-    title="Chat API with TTS",
-    description="An API that streams GPT-4 responses and converts them to speech using ElevenLabs TTS.",
+    title="AI-Assisted Genetic Diagnostics API",
+    description="An API that leverages AI to assist doctors in genetic diagnostics and interpretation of medical data.",
     version="1.0.0"
 )
 
@@ -31,14 +36,48 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/extract_letter_content")
-async def extract_letter_content():
-    with open("data/doctor_letter.txt", "r") as file:
-        content = file.read()
-        print(content)
-    return {"content": content}
+async def get_extract_letter_content():
+    return {"message": "Please use POST to upload a file"}
+
+@app.post("/extract_letter_content")
+async def extract_letter_content(file: UploadFile = File(...)):
+    # content = await file.read()
+
+    image_path = "data/doctor_letter_scan_0.jpg"
+    print(f"image_path: {image_path}")
+
+    # Getting the base64 string
+    encoded_content = utils.encode_image(image_path)
+    print(f"encoded_content: {encoded_content}")
+    
+    # encoded_content = base64.b64encode(content).decode('utf-8')
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Please transcribe the content of this image or PDF."
+                },
+                {
+                    "type": "image_url",
+                    "image_url": f"data:image/jpeg;base64,{encoded_content}"
+                }
+            ]
+        }
+    ]
+    
+    chat_response = client.chat.complete(
+        model="pixtral-12b-2409",
+        messages=messages
+    )
+    
+    transcribed_content = chat_response.choices[0].message.content
+    print(transcribed_content)
+    
+    return {"content": transcribed_content}
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 def start():
